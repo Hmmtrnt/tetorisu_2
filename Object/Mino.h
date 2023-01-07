ee00a00d9ad75a22f448afdeadee863318da7a09
廉斗 浜松 <rento5310@gmail.com>
2023-01-07 16:28:38 +0900

    ミノとステージの基盤制作途中

--- Object/Mino.h
+++ Object/Mino.h
@@ -2,7 +2,40 @@
 #pragma once
 #include "common.h"
 
+class Stage;
+
 class Mino
 {
+public:
+	Mino();
+	virtual ~Mino();
+
+	// 初期化
+	void init();
+	// 終了処理
+	void end();
+	// 更新処理
+	void update();
+	// 描画
+	void draw();
+
+	void my_make_block();
+
+	void my_draw_variable();
+
+	void my_draw_block();
+private:
+
+	int block[BLOCK_HEIGHT][BLOCK_WIDTH];
+
+	int block_x;
+
+	int block_y;
+
+
+	float block_y_count;
+
+	// ステージのポインタ
+	Stage* m_pStage;
 };
 
--- Object/Stage.h
+++ Object/Stage.h
@@ -1,5 +1,29 @@
+// ステージ
 #pragma once
+#include "common.h"
+
 class Stage
 {
+public:
+	Stage();
+	virtual ~Stage();
+
+	// 初期化処理
+	void init();
+	// 終了処理
+	void end();
+	// 更新処理
+	void update();
+	// 描画処理
+	void draw();
+
+	void my_draw_stage();
+
+	// ステージの配列
+	int stage[STAGE_HEIGHT][STAGE_WIDTH];
+
+private:
+	// 背景のハンドル
+	int m_backHandle;
 };
 
