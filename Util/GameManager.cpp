579a2ecc288c966cae4b40c38847ff725c34340d
廉斗 浜松 <rento5310@gmail.com>
2023-01-08 17:44:40 +0900

    関数のブロック追加

--- Util/GameManager.cpp
+++ Util/GameManager.cpp
@@ -1,1 +1,25 @@
 #include "GameManager.h"
+
+GameManager::GameManager()
+{
+}
+
+GameManager::~GameManager()
+{
+}
+
+void GameManager::init()
+{
+}
+
+void GameManager::end()
+{
+}
+
+void GameManager::update()
+{
+}
+
+void GameManager::draw()
+{
+}
